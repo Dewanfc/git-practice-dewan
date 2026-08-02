a97381b6ff9fbc7129cd01cf2d588df63def2b32
Dewan <150391268+Dewanfc@users.noreply.github.com>
2026-08-02 21:24:19 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+import utils
 from datetime import date
 
 print("Name: Dewan Fahad Chowdhury")
 print("Today's Date:", date.today())
+
+print("10 + 5 =", utils.add(10, 5))
+print("10 - 5 =", utils.subtract(10, 5))
